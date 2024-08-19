24ad6b726e9154014e76eeaf6e84421dd046502f
yosaka <fallingfluit@gmail.com>
2024-08-20 05:32:15 +0900

    add toggleterm.nvim

--- init.lua
+++ init.lua
@@ -544,6 +544,30 @@ require("lazy").setup({
 			end,
 			ft = { "markdown" },
 		},
+		{
+			"akinsho/toggleterm.nvim",
+			version = "*",
+			config = true,
+			init = function()
+				require("toggleterm").setup({
+					size = 100,
+					open_mapping = [[<c-t>]],
+					hide_numbers = true,
+					shade_filetypes = {},
+					shade_terminals = true,
+					shading_factor = 2,
+					start_in_insert = true,
+					insert_mappings = true,
+					persist_size = true,
+					direction = "float",
+					close_on_exit = true,
+					float_opts = {
+						border = "double",
+						-- border = 'single' | 'double' | 'shadow' | 'curved'
+					},
+				})
+			end,
+		},
 
 		-- plugins above -------------------------------------------------------------------------------------------
 	},
