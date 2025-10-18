9f1821ae9634dd2a84757b5cbb8159c9efb808a3
yosaka <fallingfluit@gmail.com>
2025-10-19 02:17:54 +0900

    bugfix: delete imacco/markdown-preview.nvim yarn install version

--- init.lua
+++ init.lua
@@ -706,23 +706,12 @@ require("lazy").setup({
 		},
 		{
 			"iamcco/markdown-preview.nvim",
-			run = "cd app && npm install",
+			-- run = "cd app && npm install",
 			cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
 			ft = { "markdown" },
 			build = function()
 				vim.fn["mkdp#util#install"]()
 			end,
-		},
-
-		-- install with yarn or npm
-		{
-			"iamcco/markdown-preview.nvim",
-			cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-			build = "cd app && yarn install",
-			init = function()
-				vim.g.mkdp_filetypes = { "markdown" }
-			end,
-			ft = { "markdown" },
 		},
 		{
 			"akinsho/toggleterm.nvim",
